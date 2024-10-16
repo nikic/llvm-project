ed15c5ed929c35a0066551c87f255ef61d43e6c2
Nikita Popov <npopov@redhat.com>
2024-10-16 14:10:31 +0200

    use context

--- llvm/lib/Transforms/InstCombine/InstCombineSimplifyDemanded.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineSimplifyDemanded.cpp
@@ -1099,6 +1099,9 @@ Value *InstCombinerImpl::SimplifyDemandedUseBits(Instruction *I,
     Known.Zero.setLowBits(Log2(Alignment));
   }
 
+  if (Depth == 0 && !I->hasOneUse())
+    computeKnownBitsFromContext(I, Known, Depth, Q);
+
   // If the client is only demanding bits that we know, return the known
   // constant. We can't directly simplify pointers as a constant because of
   // pointer provenance.
