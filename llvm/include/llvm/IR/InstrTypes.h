643aa3632c45ecb60b06d815838047a3ffbf8ca2
Nikita Popov <npopov@redhat.com>
2025-07-15 13:00:20 +0200

    add noinline

--- llvm/include/llvm/IR/InstrTypes.h
+++ llvm/include/llvm/IR/InstrTypes.h
@@ -2432,7 +2432,8 @@ private:
     return false;
   }
 
-  LLVM_ABI bool isCompatibleWithImpl(const Function *F) const;
+  LLVM_ABI LLVM_ATTRIBUTE_NOINLINE bool
+  isCompatibleWithImpl(const Function *F) const;
 };
 
 template <>
