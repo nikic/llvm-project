08491114f6fa791a776f426ca728b1406eb79b91
Wenju He <wenju.he@intel.com>
2026-04-06 14:08:15 +0200

    Extend isBasicBlockEntryGuardedByCond to handle merge block
    In a test of two consecutive loops with `eq` compare predidate, SCEV
    computes constant trip counts for them before unrolling. However, after
    unrolling the first loop, the second loop's header is a merge block with
    32 predecessors. isBasicBlockEntryGuardedByCond stops at the merge
    block, preventing SCEV from re-computing constant trip count for the
    second loop. This causes the second loop failing to fully unroll and
    a perf regression from 183K to 405K cycles on an intel gpu simulator.
    
    This PR refines the predecessor chain to handle a merge block. If all
    predecessors of the merge block are proved to be guarded by the
    condition, SCEV can re-compute constant trip count for the second loop.
    
    skip merge block if it is in a different loop
    
    update comment
    
    rename test since merge block is loop header
    
    update test: %0 -> %x

--- llvm/lib/Analysis/ScalarEvolution.cpp
+++ llvm/lib/Analysis/ScalarEvolution.cpp
@@ -12028,16 +12028,43 @@ bool ScalarEvolution::isBasicBlockEntryGuardedByCond(const BasicBlock *BB,
     PredBB = ContainingLoop->getLoopPredecessor();
   else
     PredBB = BB->getSinglePredecessor();
-  for (std::pair<const BasicBlock *, const BasicBlock *> Pair(PredBB, BB);
-       Pair.first; Pair = getPredecessorWithUniqueSuccessorForBB(Pair.first)) {
-    const CondBrInst *BlockEntryPredicate =
-        dyn_cast<CondBrInst>(Pair.first->getTerminator());
-    if (!BlockEntryPredicate)
-      continue;
-
-    if (ProveViaCond(BlockEntryPredicate->getCondition(),
-                     BlockEntryPredicate->getSuccessor(0) != Pair.second))
-      return true;
+  // For simplificity, only one merge block is handled.
+  const BasicBlock *MergeBlock = nullptr;
+  unsigned NumVisits = 0;
+  auto ProveViaPredecessorChain = [&](const BasicBlock *Pred,
+                                      const BasicBlock *Succ,
+                                      unsigned MaxNumVisits) {
+    for (std::pair<const BasicBlock *, const BasicBlock *> Pair(Pred, Succ);
+         Pair.first;
+         Pair = getPredecessorWithUniqueSuccessorForBB(Pair.first)) {
+      if (NumVisits++ > MaxNumVisits)
+        return false;
+      const CondBrInst *BlockEntryPredicate =
+          dyn_cast<CondBrInst>(Pair.first->getTerminator());
+      if (BlockEntryPredicate &&
+          ProveViaCond(BlockEntryPredicate->getCondition(),
+                       BlockEntryPredicate->getSuccessor(0) != Pair.second))
+        return true;
+      if (!MergeBlock && Pair.first->hasNPredecessorsOrMore(2))
+        MergeBlock = Pair.first;
+    }
+    return false;
+  };
+  unsigned MaxChainVisits = ~0u; // No limit for the first predecessor chain.
+  if (ProveViaPredecessorChain(PredBB, BB, MaxChainVisits))
+    return true;
+  if (MergeBlock) {
+    // The Chain from MergeBlock to BB consists only of single-predecessor
+    // blocks. If MergeBlock is not in a different loop, and every predecessor
+    // of MergeBlock proves the condition, then BB is guarded by the condition.
+    MaxChainVisits = 128;
+    auto ProveMergeBlockPredecessor = [&](const BasicBlock *Pred) {
+      return ProveViaPredecessorChain(Pred, MergeBlock, MaxChainVisits);
+    };
+    const Loop *MergeBlockLoop = LI.getLoopFor(MergeBlock);
+    if (!MergeBlockLoop || ContainingLoop == MergeBlockLoop)
+      if (all_of(predecessors(MergeBlock), ProveMergeBlockPredecessor))
+        return true;
   }
 
   // Check conditions due to any @llvm.assume intrinsics.
